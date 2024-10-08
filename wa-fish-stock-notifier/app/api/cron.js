23493eddeb6f0ffbf498d8dc26224516b6bf726a
Brad Hendrickson <brad.hendrickson@silvertrek.net>
2024-10-07 20:25:41 -0700

    add logging

--- wa-fish-stock-notifier/app/api/cron.js
+++ wa-fish-stock-notifier/app/api/cron.js
@@ -1,6 +1,7 @@
 import { ProcessSubscriptionsServer } from './subscriptions';
 
 export default async function handler(req, res) {
+    console.log('Processing Subscriptions Server');
     ProcessSubscriptionsServer();
     res.status(200).json({ message: 'Process Subscriptions Server' });
 }
