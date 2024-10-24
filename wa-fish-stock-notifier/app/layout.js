fdea5f0c2fa06807e0c007bf0441bcdec10b8102
Brad Hendrickson <brad.hendrickson@silvertrek.net>
2024-10-24 10:56:43 -0700

    add analytics

--- wa-fish-stock-notifier/app/layout.js
+++ wa-fish-stock-notifier/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = localFont({
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
